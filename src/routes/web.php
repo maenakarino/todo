523c63dcd4a452750b892c24b4eb0637222131cc
maenaka rino <rino.peach.7@gmail.com>
2024-05-29 11:25:50 +0900

    リモートリポジトリの変更

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Todocontroller;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/', [Todocontroller::class,'index']);
 Route::post('/todos', [Todocontroller::class, 'store']);
+Route::patch('/todos/update', [Todocontroller::class, 'update']);
+Route::delete('/todos/delete', [Todocontroller::class, 'destroy']);
+
+Route::get('/categories', [CategoryController::class, 'index']);
