cdc0e94d51b2fda7048b75fba915f5e17d119b09
maenaka rino <rino.peach.7@gmail.com>
2024-05-30 23:18:55 +0900

    リモートリポジトリの変更

--- src/app/Http/Requests/CategoryRequest.php
+++ src/app/Http/Requests/CategoryRequest.php
@@ -6,27 +6,35 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class CategoryRequest extends FormRequest
 {
-    
-    public function authorize()
-    {
-        return true;
-    }
+  /**
+   * Determine if the user is authorized to make this request.
+   *
+   * @return bool
+   */
+  public function authorize()
+  {
+    return true;
+  }
 
-    
-    public function rules()
-    {
-        return [
-            "name" => ['required', 'string', 'max:10', 'unique:categories'],
-        ];
-    }
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array
+   */
+  public function rules()
+  {
+    return [
+      'name' => ['required', 'string', 'max:10', 'unique:categories'],
+    ];
+  }
 
-    public function messages()
-    {
-        return [
-            'name.required' => 'カテゴリーを入力してください',
-            'name.string' => 'カテゴリーを文字列で入力してください',
-            'name.max' => 'カテゴリーを10文字以下で入力してください',
-            'name.unique' => 'カテゴリーが既に存在しています',
-        ];
-    }
+  public function messages()
+  {
+    return [
+      'name.required' => 'カテゴリを入力してください',
+      'name.string' => 'カテゴリを文字列で入力してください',
+      'name.max' => 'カテゴリを10文字以下で入力してください',
+      'name.unique' => 'カテゴリが既に存在しています',
+    ];
+  }
 }
